750e3a56f7bd853daaccaf0a5fa249563600aad1
Nicholas Ramsay <nicholasramsay@hotmail.com>
2022-03-14 05:01:54 -0700

    - added in a settings menu in the title screen - all it does right now is control the volume of the sounds lol

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+     AudioMixer masterMixer;
+    [SerializeField]
+    private GameObject soundSlider;
+    [SerializeField]
+    private GameObject settingsContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        settingsContainer.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetSoundSlider()
+    {
+        if (soundSlider.GetComponent<Slider>().value == soundSlider.GetComponent<Slider>().minValue)
+        {
+            masterMixer.SetFloat("soundsVol", -1000);
+        }
+
+        else
+        {
+            masterMixer.SetFloat("soundsVol", soundSlider.GetComponent<Slider>().value);
+        }
+    }
+
+    public void TurnOn()
+    {
+        UIManager.Instance.TurnOn(transform.GetChild(0).gameObject);
+    }
+
+    public void TurnOff()
+    {
+        UIManager.Instance.TurnOff(transform.GetChild(0).gameObject);
+    }
+      
+}
