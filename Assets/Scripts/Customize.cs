df2bcf60d0aa80d2f63b9a2f5902f30b05e9b57e
Nicholas Ramsay <nicholasramsay@hotmail.com>
2022-03-19 05:33:09 -0700

    - created some cameras in the customize character screen

--- Assets/Scripts/Customize.cs
+++ Assets/Scripts/Customize.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Customize : MonoBehaviour
 {
@@ -8,12 +9,19 @@ public class Customize : MonoBehaviour
     private GameObject customizeCamera;
     [SerializeField]
     private GameObject startMenu;
+    [SerializeField]
     private GameObject score;
+    [SerializeField]
+    private GameObject hatsCamPosition;
+    [SerializeField]
+    private GameObject bodyCamPosition;
 
     private void Awake()
     {
         startMenu = GameObject.Find("StartMenu").gameObject;
         score = GameObject.Find("Score").gameObject;
+        hatsCamPosition = GameObject.Find("HatsCamPosition").gameObject;
+        bodyCamPosition = GameObject.Find("BodyCamPosition").gameObject;
     }
 
     void Start()
@@ -42,5 +50,17 @@ public class Customize : MonoBehaviour
         transform.GetChild(0).gameObject.SetActive(false);
         startMenu.SetActive(true);
         score.SetActive(true);
+    }
+
+    public void TransitionHats()
+    {
+        //customizeCamera.transform.position = hatsCamPosition.transform.position;
+        customizeCamera.transform.DOMove(hatsCamPosition.transform.position, .5f, false);
+    }
+
+    public void TransitionBody()
+    {
+        //customizeCamera.transform.position = bodyCamPosition.transform.position;
+        customizeCamera.transform.DOMove(bodyCamPosition.transform.position, .5f, false);
     }
 }
