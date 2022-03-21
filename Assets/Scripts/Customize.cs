fd4fd0ef28bbe6412015c88fd50ffb24ec5c8bd8
Nicholas Ramsay <nicholasramsay@hotmail.com>
2022-03-21 16:08:56 -0700

    - customization UI navigation working as intended.

--- Assets/Scripts/Customize.cs
+++ Assets/Scripts/Customize.cs
@@ -15,18 +15,29 @@ public class Customize : MonoBehaviour
     private GameObject hatsCamPosition;
     [SerializeField]
     private GameObject bodyCamPosition;
+    [SerializeField]
+    private GameObject defaultCamPosition;
+    [SerializeField]
+    private GameObject selectionArrows;
+    [SerializeField]
+    private GameObject hatsBtn;
+    [SerializeField]
+    private GameObject bodyBtn;
+    private bool isCustomizing;
+    
 
     private void Awake()
     {
-        startMenu = GameObject.Find("StartMenu").gameObject;
-        score = GameObject.Find("Score").gameObject;
-        hatsCamPosition = GameObject.Find("HatsCamPosition").gameObject;
-        bodyCamPosition = GameObject.Find("BodyCamPosition").gameObject;
+        //startMenu = GameObject.Find("StartMenu").gameObject;
+        //score = GameObject.Find("Score").gameObject;
+        //hatsCamPosition = GameObject.Find("HatsCamPosition").gameObject;
+       // bodyCamPosition = GameObject.Find("BodyCamPosition").gameObject;
     }
 
     void Start()
     {
-        
+        selectionArrows.SetActive(false);
+        isCustomizing = false;
     }
 
     // Update is called once per frame
@@ -41,7 +52,6 @@ public class Customize : MonoBehaviour
         transform.GetChild(0).gameObject.SetActive(true);
         startMenu.SetActive(false);
         score.SetActive(false);
-
     }
 
     public void TurnOff()
@@ -52,15 +62,44 @@ public class Customize : MonoBehaviour
         score.SetActive(true);
     }
 
+    public void GoBack()
+    {
+        customizeCamera.transform.DOMove(defaultCamPosition.transform.position, .5f, false);
+        hatsBtn.SetActive(true);
+        bodyBtn.SetActive(true);
+        selectionArrows.SetActive(false);
+        isCustomizing = false;
+    }
+
+    public void CustomizeBackButton()
+    {
+        if (isCustomizing)
+        {
+            GoBack();
+        }
+        else
+        {
+            TurnOff();
+        }
+    }
+
     public void TransitionHats()
     {
         //customizeCamera.transform.position = hatsCamPosition.transform.position;
         customizeCamera.transform.DOMove(hatsCamPosition.transform.position, .5f, false);
+        hatsBtn.SetActive(false);
+        bodyBtn.SetActive(false);
+        selectionArrows.SetActive(true);
+        isCustomizing = true;
     }
 
     public void TransitionBody()
     {
         //customizeCamera.transform.position = bodyCamPosition.transform.position;
         customizeCamera.transform.DOMove(bodyCamPosition.transform.position, .5f, false);
+        hatsBtn.SetActive(false);
+        bodyBtn.SetActive(false);
+        selectionArrows.SetActive(true);
+        isCustomizing = true;
     }
 }
